0c69ddb37907994ddae6b67d02d6892bdea31470
MOIZ777440 <147206684+MOIZ777440@users.noreply.github.com>
2025-03-25 23:11:03 -0400

    Update main.c
    added missing functions for notification module

--- main.c
+++ main.c
@@ -14,16 +14,19 @@ int main() {
         printf("2. Display Tasks\n");
         printf("3. Edit Task\n");
         printf("4. Delete Task\n");
-        printf("5. Exit\n");
+        printf("5. Due Tasks\n");
+        printf("6. Snooze Task Notification\n");
+        printf("7. View Notification Log\n");
+        printf("8. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        getchar(); 
+        getchar();
 
         switch (choice) {
             case 1:
                 printf("Enter Title: ");
                 fgets(title, sizeof(title), stdin);
-                strtok(title, "\n"); 
+                strtok(title, "\n");
                 printf("Enter Description: ");
                 fgets(description, sizeof(description), stdin);
                 strtok(description, "\n");
@@ -32,7 +35,6 @@ int main() {
                 strtok(deadline, "\n");
 
                 addTask(title, description, deadline);
-                
                 sendNotification("New task added!", title);
                 break;
 
@@ -56,22 +58,41 @@ int main() {
                 strtok(deadline, "\n");
 
                 editTask(id, title, description, deadline);
-                
-                
                 sendNotification("Task updated!", title);
                 break;
 
             case 4:
                 printf("Enter Task ID to delete: ");
                 scanf("%d", &id);
-                
                 deleteTask(id);
-
-                
                 sendNotification("Task deleted!", "Task Management System");
                 break;
 
             case 5:
+                checkDeadlines(taskList, taskCount);
+                break;
+
+            case 6:
+                printf("Enter Task ID to snooze: ");
+                scanf("%d", &id);
+                getchar();
+
+                if (id >= 0 && id < taskCount) {
+                    int mins;
+                    printf("Enter snooze time in minutes: ");
+                    scanf("%d", &mins);
+                    getchar();
+                    snoozeNotification(&taskList[id], mins);
+                } else {
+                    printf("Invalid Task ID!\n");
+                }
+                break;
+
+            case 7:
+                printNotificationLog();
+                break;
+
+            case 8:
                 printf("Exiting...\n");
                 return 0;
 
