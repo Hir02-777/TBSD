55262fad1e002982a2b04cf6ef773d2b311898d2
MOIZ777440 <147206684+MOIZ777440@users.noreply.github.com>
2025-03-25 23:03:29 -0400

    Update notification.h

--- notification.h
+++ notification.h
@@ -1,1 +1,29 @@
-#pragma once
+
+#ifndef NOTIFICATION_H
+#define NOTIFICATION_H
+
+#include "task_manager.h"
+#include <time.h>
+
+typedef enum {
+    SENT,
+    SNOOZED,
+    DISMISSED
+} NotificationStatus;
+
+typedef struct {
+    Task task;
+    NotificationStatus status;
+    char timestamp[64];
+} NotificationLog;
+
+void checkDeadlines(Task* taskList, int taskCount);
+void sendNotification(const char* title, const char* message);
+void snoozeNotification(Task* task, int minutes);
+void dismissNotification(Task* task);
+void logNotification(Task task, NotificationStatus status);
+int isTaskDue(Task task);
+void printNotificationLog(void);
+
+
+#endif
